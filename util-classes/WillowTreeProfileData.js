f093dc4fd96598fe4778f0d551ed6b8a9106a544
Stuart Himmer <stuart.himmer@clearlink.com>
2018-08-27 21:27:34 -0600

    cleaned up commented out code

--- util-classes/WillowTreeProfileData.js
+++ util-classes/WillowTreeProfileData.js
@@ -15,18 +15,14 @@ class WillowTreeProfileData{
                 callback(error,response.statusCode)
             } else{
                 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                // console.log('body:', body); // Print the response
+
                 callback(null,body);
             }
         });
     }
 
     save_data(data, callback){
-        // let file_key = {
-        //     Bucket: 'willow-tree',
-        //     Key: 'profile_data.json',
-        //     Body: data
-        // };
+
 
         if (typeof(data) == 'string') {
             data = JSON.parse(data)
@@ -56,17 +52,12 @@ class WillowTreeProfileData{
                 , data[i].headshot.alt
                 , data[i].headshot.height
                 , data[i].headshot.width]);
-            // // sociallinks table insert
-            // social_links_data.push([data[i].socialLinks.type
-            //     , data[i].socialLinks.callToAction
-            //     , data[i].socialLinks.url ]);
             
         }
-        // console.log(people_data);
+
         let people_insert = "INSERT INTO peoples (id, type, slug, job_title, first_name, last_name, bio) VALUES ?";
         let headshot_insert = "INSERT INTO headshots (people_id, type, mime_type, image_id, url, alt, height, width) VALUES ?";
-        // let social_links_insert = "INSERT INTO social_links (people_id, type, call_to_action, url) VALUES ?";
-        // console.log(people_insert);
+
         this.mysql.query_with_values(people_insert, [people_data], function(err, result){
             if (err) {
                 callback(err);
@@ -80,29 +71,11 @@ class WillowTreeProfileData{
             } else {
                callback(null, 'Data loaded into mysql'); 
             }
-        });
-        
-        // this.mysql.insert(social_links_insert, [social_links_data], function(err, result){
-        //     if (err) {
-        //         callback(err);
-        //     }
-            
-        // });   
-          
+        });  
         
     }
-
 
 }
 
 
 module.exports = WillowTreeProfileData;
-
-// var request = require('request');
-// var wt = new WillowTreeProfileData(request); 
-// wt.get_data('https://www.willowtreeapps.com/api/v1.0/profiles', function(err, r){
-//     wt.save_data(r, function (err, result) {
-//         console.log(err);
-//         console.log(result);
-//     });
-// });
